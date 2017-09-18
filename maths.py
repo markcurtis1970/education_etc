de60341638a6d67ee0f4434c0c6ebf7c9cf0488c
Mark Curtis <mark@Homer.cable.virginmedia.net>
2017-09-18 13:13:19 +0100

    typo

--- maths.py
+++ maths.py
@@ -4,7 +4,7 @@ times_number = input("Enter times table number: ")
 times_length = input("Enter times table length: ")
 
 # Execute this loop up to the length we want for the times table
-# For example is we are doing the 5 times table up to 15 then the
+# For example if we are doing the 5 times table up to 15 then the
 # length will be 15. Note the range function needs to be increased
 # by 1 in order to get the full range
 for multiplier in range(1,times_length + 1):
