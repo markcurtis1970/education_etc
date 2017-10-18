8f01180c897f53a2feb1f05b11fa3eaa646af6a1
markcurtis1970 <markcurtis1970@gmail.com>
2017-10-18 10:16:57 +0100

    added optional arguments

--- maths.py
+++ maths.py
@@ -1,7 +1,14 @@
+#!/usr/bin/python
 # Simple example to show how to calculate a times
 # table for a given number and a given length
-times_number = input("Enter times table number: ")
-times_length = input("Enter times table length: ")
+import sys
+
+if len(sys.argv) < 3:
+    times_number = input("Enter times table number: ")
+    times_length = input("Enter times table length: ")
+else:
+    times_number = int(sys.argv[1])
+    times_length = int(sys.argv[2])
 
 # Execute this loop up to the length we want for the times table
 # For example if we are doing the 5 times table up to 15 then the
